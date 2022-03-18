899b5738c792f9fba9bc63bb3c450b40a9718f6e
Abid Ali <abdsoftfsd@gmail.com>
2022-03-18 08:36:05 +0500

    Understanding exceptions in python

--- 51_exceptions/exceptions.py
+++ 51_exceptions/exceptions.py
@@ -1,1 +1,35 @@
 # Understanding Exceptions
+DIGIT_MAP = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+x = -1
+
+
+def convert(s):
+    try:
+        number = ''
+        for token in s:
+            number += DIGIT_MAP[token]
+        x = int(number)
+    except (KeyError, TypeError):
+        pass  # pass is a no-op i.e. does nothing. It is to construct empty blocks to avoid runtime errors
+    return x
+
+
+convert("one three around grillion".split())
+
+convert("one three three two".split())
+
+convert(512)
+
+convert("elephant".split())
