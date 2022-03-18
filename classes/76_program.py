1aa86d3caf499d0e8178aff6ad3586109ee9f8ef
Abid Ali <abdsoftfsd@gmail.com>
2022-03-18 21:47:08 +0500

    Implementing a class for Airflight booking

---
+++ classes/76_program.py
@@ -0,0 +1,7 @@
+from classes import *
+
+f = Flight("BA758", Aircraft(registration="G-EUPT", model="Airbus A139", num_rows=22, num_seats_per_row=6))
+
+f.allocate_seat('1A', 'Abid Ali')
+f.allocate_seat('1B', 'Sajid Ali')
+f._seating
