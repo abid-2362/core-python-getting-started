4500b15d75e4acc94275aba66c06ddc42c6f008a
Abid Ali <abdsoftfsd@gmail.com>
2022-03-19 08:01:59 +0500

    Understanding Inheritance and Derived Classes

--- classes/airtravel.py
+++ classes/airtravel.py
@@ -101,13 +101,23 @@ class Flight:
                     yield passenger, f"{row}{letter}"
 
 
-class AirbusA319:
+# Inheritance
+# Thanks to duck-typing, Python uses inheritance less than many other languages.
+class Aircraft:
 
     def __init__(self, registration):
         self._registration = registration
 
     def registration(self):
         return self._registration
+
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+
+
+# Derived Class AirbusA319 derived from Aircraft class
+class AirbusA319(Aircraft):
 
     def model(self):
         return "Airbus A319"
@@ -116,13 +126,7 @@ class AirbusA319:
         return range(1, 23), "ABCDEF"
 
 
-class Boeing777:
-
-    def __init__(self, registration):
-        self._registration = registration
-
-    def registration(self):
-        return self._registration
+class Boeing777(Aircraft):
 
     def model(self):
         return "Boeing 777"
